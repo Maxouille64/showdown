4f12a9616fc1e9fffd87878f4ceff5953a3ee2bd
Maxouille <xd.maxouille@gmail.com>
2020-05-13 13:12:38 +0200

    Update websocket_client.py

--- showdown/websocket_client.py
+++ showdown/websocket_client.py
@@ -25,18 +25,199 @@ class PSWebsocketClient:
     password = None
     last_message = None
     last_challenge_time = 0
+    avatar = None
+    room = None
 
     @classmethod
-    async def create(cls, username, password, address):
+    async def create(cls, username, password, address, avatar, room):
         self = PSWebsocketClient()
         self.username = username
         self.password = password
         self.address = "ws://{}/showdown/websocket".format(address)
         self.websocket = await websockets.connect(self.address)
         self.login_uri = "https://play.pokemonshowdown.com/action.php"
+        self.avatar = avatar
+        self.room = room
         return self
 
     async def receive_message(self):
+        message = await self.websocket.recv()
+        if message.startswith(">" + self.room):
+            msg = message.split("|")
+            message = message.replace(msg[0], '')
+        return message
+
+    async def send_message(self, room, message_list):
+        message = room + "|" + "|".join(message_list)
+        await self.websocket.send(message)
+        self.last_message = message
+        logger.debug(message)
+
+
+    async def get_id_and_challstr(self):
+        while True:
+            message = await self.receive_message()
+            split_message = message.split('|')
+            if split_message[1] == 'challstr':
+                return split_message[2], split_message[3]
+
+    async def login(self):
+        logger.debug("Logging in...")
+        client_id, challstr = await self.get_id_and_challstr()
+        if self.password:
+            response = requests.post(
+                self.login_uri,
+                data={
+                    'act': 'login',
+                    'name': self.username,
+                    'pass': self.password,
+                    'challstr': "|".join([client_id, challstr])
+                }
+            )
+
+        else:
+            response = requests.post(
+                self.login_uri,
+                data={
+                    'act': 'getassertion',
+                    'userid': self.username,
+                    'challstr': '|'.join([client_id, challstr]),
+                }
+            )
+
+        if response.status_code == 200:
+            if self.password:
+                response_json = json.loads(response.text[1:])
+                assertion = response_json.get('assertion')
+            else:
+                assertion = response.text
+
+            message = ["/trn " + self.username + ",0," + assertion]
+            logger.debug("Successfully logged in")
+            await self.send_message('', message)
+            message = ["/avatar " + self.avatar]
+            logger.debug("avatar: " + self.avatar)
+            await self.send_message('', message)
+            message = ["/j " + self.room]
+            logger.debug("j'ai rejoin " + self.room)
+            await self.send_message('', message)
+        else:
+            logger.error("Could not log-in\nDetails:\n{}".format(response.content))
+            raise LoginError("Could not log-in")
+
+    async def update_team(self, team):
+        message = ["/utm {}".format(team)]
+        await self.send_message('', message)
+
+    async def challenge_user(self, user_to_challenge, battle_format, team):
+        logger.debug("Challenging {}...".format(user_to_challenge))
+        if time.time() - self.last_challenge_time < 10:
+            logger.info("Sleeping for 10 seconds because last challenge was less than 10 seconds ago")
+            await asyncio.sleep(10)
+        await self.update_team(team)
+        message = ["/challenge {},{}".format(user_to_challenge, battle_format)]
+        await self.send_message('', message)
+        self.last_challenge_time = time.time()
+
+    async def accept_challenge(self, battle_format, team):
+        logger.debug("Waiting for a {} challenge".format(battle_format))
+        await self.update_team(team)
+        username = None
+        while username is None:
+            msg = await self.receive_message()
+            split_msg = msg.split('|')
+            if split_msg[1] == 'updatechallenges':
+                try:
+                    challenges = json.loads(split_msg[2])
+                    if challenges['challengesFrom'] is not None:
+                        print(challenges)
+                        username, challenge_format = next(iter(challenges['challengesFrom'].items()))
+                        if challenge_format != battle_format:
+                            username = None
+                except ValueError:
+                    username = None
+                except StopIteration:
+                    username = None
+
+        message = ["/accept " + username]
+        await self.send_message('', message)
+
+    async def tour_join(self, battle_format, team):
+        while True:
+            msg = await self.receive_message()
+            split_msg = msg.split('|')
+            print(msg)
+            if msg.startswith("|tournament|create|"):
+                    message = ["/tour join"]
+                    await self.send_message(self.room, message)
+                    format = split_msg[3]
+                    message = ["nouveau tournois en " + format]
+                    logger.debug(message)
+                    await self.send_message(self.room, message)
+                    await self.update_team(team)
+            if msg.startswith("|tournament|update|"):
+                    if "challenged" in msg:
+                        oppo = json.loads(split_msg[3])
+                        username = str(oppo["challenged"])
+                        message = ["/tournament acceptchallenge " + username]
+                        await self.send_message(self.room, message)
+                    if "challenges" in msg:
+                        oppo = json.loads(split_msg[3])
+                        username = str(oppo["challenges"])
+                        message = ["/tour challenge " + username]
+                        await self.send_message(self.room, message)
+                    if "challengeBys" in msg:
+                        oppo = json.loads(split_msg[3])
+                        username = str(oppo["challengeBys"])
+                        message = ["/tournament challenge " + username]
+                        await self.send_message(self.room, message)
+                        message = ["/tournament acceptchallenge " + username]
+                        await self.send_message(self.room, message)
+            if msg.startswith("|updatesearch|") and "battle" in msg:
+                break
+
+
+    async def search_for_match(self, battle_format, team):
+        logger.debug("Searching for ranked {} match".format(battle_format))
+        await self.update_team(team)
+        message = ["/search {}".format(battle_format)]
+        await self.send_message("", message)
+
+
+    async def leave_battle(self, battle_tag, save_replay=False):
+        if save_replay:
+            await self.save_replay(battle_tag)
+
+
+        message = ["/leave {}".format(battle_tag)]
+        await self.send_message('', message)
+
+        while True:
+            msg = await self.receive_message()
+            if battle_tag in msg and 'deinit' in msg:
+                return
+
+    async def save_replay(self, battle_tag):
+        message = ["/savereplay"]
+        await self.send_message(battle_tag, message)
+
+        while True:
+            msg = await self.receive_message()
+            if msg.startswith("|queryresponse|savereplay|"):
+                obj = json.loads(msg.replace("|queryresponse|savereplay|", ""))
+                log = obj['log']
+                identifier = obj['id']
+                post_response = requests.post(
+                    "https://play.pokemonshowdown.com/~~showdown/action.php?act=uploadreplay",
+                    data={
+                        "log": log,
+                        "id": identifier
+                    }
+                )
+                if post_response.status_code != 200:
+                    raise SaveReplayError("POST to save replay did not return a 200: {}".format(post_response.content))
+                break
+
         message = await self.websocket.recv()
         return message
 
